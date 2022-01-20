95e7581a9f204941f8da740350bd75113623452c
TheSamuelBartek <82447376+TheSamuelBartek@users.noreply.github.com>
2022-01-20 14:08:37 +0100

    yes

---
+++ src/test/java/com/example/Hellothere/HelloThereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Hellothere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloThereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
