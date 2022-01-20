4fbc4c35edd42b6b6a2eb59ab6003cec2a5161ce
TheSamuelBartek <82447376+TheSamuelBartek@users.noreply.github.com>
2022-01-20 20:18:37 +0100

    a

--- src/main/java/com/example/Hellothere/HelloController.java
+++ src/main/java/com/example/Hellothere/HelloController.java
@@ -3,16 +3,14 @@ package com.example.Hellothere;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/")
-    public String index() {
-        System.out.println("hello there!!");
-
-        return "index";
-
+    public ModelAndView index() {
+        return new ModelAndView("frontpage.html");
     }
 
 }
