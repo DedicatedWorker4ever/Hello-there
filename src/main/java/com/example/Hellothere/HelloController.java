c8f9ac5f18bff999a253052811fa8090700b7dd8
TheSamuelBartek <82447376+TheSamuelBartek@users.noreply.github.com>
2022-01-20 20:37:12 +0100

    a

--- src/main/java/com/example/Hellothere/HelloController.java
+++ src/main/java/com/example/Hellothere/HelloController.java
@@ -1,6 +1,6 @@
 package com.example.Hellothere;
 
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -9,8 +9,8 @@ import org.springframework.web.servlet.ModelAndView;
 public class HelloController {
 
     @GetMapping("/")
-    public ModelAndView index() {
-        return new ModelAndView("frontpage.html");
+    public String index() {
+        return "Hello There!";
     }
 
 }
--- src/main/java/com/example/Hellothere/HelloThereApplication.java
+++ src/main/java/com/example/Hellothere/HelloThereApplication.java
@@ -1,7 +1,11 @@
 package com.example.Hellothere;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class HelloThereApplication {
@@ -9,5 +13,7 @@ public class HelloThereApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloThereApplication.class, args);
 	}
+
+
 
 }
